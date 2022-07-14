9969aa2e1419d26f9b219bfc0faf930d30e11933
aleksandr-suchugov <alex.coup@yandex.ru>
2022-07-14 18:29:49 +0300

    onload eventListner added

--- src/js/GoblinGame.js
+++ src/js/GoblinGame.js
@@ -30,10 +30,10 @@ export default class GoblinGame {
   }
 
   imgCreate(interval) {
-    // const goblin = document.createElement('img');
-    // goblin.src = './assets/goblin.png';  **выпадает ошибка 404 при попытке загрузить картинку при развертывании проекта
-    const goblin = document.getElementById('x01');
-    goblin.classList.remove('hidden');
+    const goblin = document.createElement('img');
+    goblin.src = './assets/goblin.png';  //выпадает ошибка 404 при попытке загрузить картинку при развертывании проекта
+    // const goblin = document.getElementById('x01');
+    // goblin.classList.remove('hidden');
       setInterval(() => {
         const position = this.goblinSetter();
         this.field.childNodes[position].appendChild(goblin);
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import '../css/style.css';
 import GoblinGame from './GoblinGame';
 import 'bootstrap';
 
-const goblinGame = new GoblinGame();
-goblinGame.boardGeneration(4);
-goblinGame.imgCreate(2000);
+window.addEventListener("load", () => {
+    const goblinGame = new GoblinGame();
+    goblinGame.boardGeneration(4);
+    goblinGame.imgCreate(2000);
+});
