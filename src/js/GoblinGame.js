b8e06f3fca801a241ed2e4d69ce667b2b3c2c251
aleksandr-suchugov <alex.coup@yandex.ru>
2022-07-24 01:14:43 +0300

    3rd commit

--- src/js/GoblinGame.js
+++ src/js/GoblinGame.js
@@ -5,6 +5,9 @@ export default class GoblinGame {
     this.position = -1;
     this.field = 0;
     this.boarSize = 1;
+    this.successShot = 0;
+    this.bossShot = 0;
+    this.rounds = 0;
   }
 
   goblinSetter() {
@@ -28,17 +31,32 @@ export default class GoblinGame {
       cell.classList.add('cell');
       this.field.appendChild(cell);
     }
+    const gameResults = document.createElement('p');
+    gameResults.classList.add('score');
+    documen.body.insertAdjacentHTML('beforeEnd', gameResults);
   }
 
   imgCreate(interval) {
     const goblin = new Image();
-    // const goblin = document.createElement('img');
     goblin.src = image;
-    // const goblin = document.getElementById('x01');
-    // goblin.classList.remove('hidden');
+    const gameResults = document.getElementsByClassName('score');
     setInterval(() => {
       const position = this.goblinSetter();
       this.field.childNodes[position].appendChild(goblin);
+      this.rounds++;
+      gameResults.textContent = `Попадания: ${this.successShot} / Промахи: ${this.bossShot} / Число попыток: ${this.rounds}`;
     }, interval);
   }
+
+  shootsRecorder() {
+    const board = document.getElementsByClassName('board');
+    board.addEventListner('click', (ev) => {
+      ev.currentTarget.parantElement === board ? this.bossShot++ : this.successShot++;
+    })
+    if (this.bossShot === 5) {
+      alert('Поражение((');
+      return location.reload();
+    }
+  }
+
 }
